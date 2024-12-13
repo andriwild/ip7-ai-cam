da369b3d25b1b94716354e5b234b6b55b74d4733
Andri Wild <andri.wild@students.fhnw.ch>
2024-12-13 16:52:04 +0100

    fix wrong import

--- camera_adapter/impl/PiCamera.py
+++ camera_adapter/impl/PiCamera.py
@@ -4,7 +4,7 @@ from camera_adapter.ICamera import ICamera
 
 class PiCamera(ICamera):
     def __init__(self, width=640, height=480):
-        from camera_adapter.picamera2 import Picamera2
+        from picamera2 import Picamera2
         self._camera = Picamera2()
         self._camera.configure(
                 self._camera.create_preview_configuration(
