7ad472337cc7f675856babeffbe6ccb797348203
Andri Wild <andri.wild@students.fhnw.ch>
2024-12-14 17:03:18 +0100

    add screensaver to static frame generator

--- camera_adapter/impl/static.py
+++ camera_adapter/impl/static.py
@@ -1,28 +1,60 @@
 import logging
 import numpy as np
-from time import sleep
+import cv2
+import time
 
 from camera_adapter.interface.camera import Camera
 
 logger = logging.getLogger(__name__)
 
+
 class StaticFrameGenerator(Camera):
 
     NAME = "static"
 
-    def __init__(self, width=640, height=480):
-        self.static_frame = np.random.randint(0, 10, (width, height, 3), dtype=np.uint8)
+    def __init__(self, width: int = 640, height: int = 480):
+        logger.info("Initializing StaticFrameGenerator")
+        self.width = width
+        self.height = height
+        self.frame_counter = 0
+        self.bee_position = [self.width // 2, self.height // 2]  # Initial bee position
+        self.direction = [5, 3]  # Movement direction
+
+    def generate_screensaver_frame(self):
+        logger.debug("Generating initial screensaver frame")
+        frame = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+        border = 30
+        cv2.rectangle(frame, (border, border), (self.width - border, self.height - border), (0, 255, 0), 2)
+        return frame
+
+    def update_bee_position(self):
+        for i in range(2):
+            self.bee_position[i] += self.direction[i]
+            if self.bee_position[i] <= 50 or self.bee_position[i] >= [self.width, self.height][i] - 50:
+                self.direction[i] = -self.direction[i]  # Reverse direction on collision
 
     def get_frame(self):
-        sleep(0.5)
-        logger.debug("Getting frame from static frame generator")
-        return self.static_frame
+        logger.debug("Getting frame from StaticFrameGenerator")
+        frame = self.generate_screensaver_frame()
+        self.update_bee_position()
+
+        # Draw a moving bee (circle) on the frame
+        cv2.circle(
+            frame, 
+            tuple(self.bee_position), 
+            20, 
+            (0, 255, 255), 
+            -1  # Filled circle
+        )
+        self.frame_counter += 1
+        time.sleep(0.05)  # Simulate delay
+        return frame
 
     def release(self):
-        logger.info("Releasing static frame generator")
+        logger.info("Releasing StaticFrameGenerator")
         pass
 
     def get_name(self) -> str:
-        logger.debug("Getting camera name for static frame generator")
+        logger.debug("Getting camera name for StaticFrameGenerator")
         return self.NAME
 
