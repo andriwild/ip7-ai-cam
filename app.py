1f9792f0765fcb43721c86297fab6acc99c790cc
Andri Wild <andri.wild@students.fhnw.ch>
2025-01-10 19:28:17 +0100

    pipeline updates are set by setter methods

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import argparse
 import logging
 import yaml
+from collections import defaultdict
 from queue import Queue
 from pprint import pprint
+from typing import Any
 
-from config.configServer import ConfigServer
-from config.config import ConfigManager
+from config.configServer import PipelineConfigurator
 from pipeline.pipeline import Pipeline
+from utilities.classLoader import ClassLoader
 
 logging.basicConfig(
     level=logging.INFO,
@@ -16,36 +18,37 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def load_config(file_path: str):
-    logger.info(f"Loading settings from {file_path}")
-    config = yaml.safe_load(open(file_path))
+def instances_from_config(config):
+    categories = config.keys()
 
-    settings = {
-        "sinks": config.get("sinks"),
-        "steps": config.get("steps"),
-        "sources": config.get("sources"),
-    }
-    return settings
+    instances = defaultdict(dict)
 
+    for category in categories:
+        class_configs = config.get(category, [])
+        for class_config in class_configs:
+            instance = ClassLoader.instantiate_class(class_config)
+            instances[category][class_config["name"]] = instance
 
-def main(config_file: str)-> None:
+    return instances
+
+    
+def main(config_file: str, host: str, port: int)-> None:
     logger.info(f"Start Edge ML Pipeline")
-    config = load_config(config_file)
-    pprint(config)
+    config = yaml.safe_load(open(config_file))
+    instances = instances_from_config(config)
 
+    queue  = Queue(maxsize=1)
+    pipeline = Pipeline(queue, instances)
+    PipelineConfigurator(pipeline, config, host, port)
 
-    manager = ConfigManager(config)
-    ConfigServer(manager, config, "0.0.0.0", 8001)
+    pipeline.run_forever() # doesn't return
 
-    queue  = Queue(maxsize=10)
-    pipeline = Pipeline(queue=queue)
-
-    manager.attach(pipeline)
-    pipeline.run_forever()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config", type=str, default="config.yml", help="config file to load configuration")
+    parser.add_argument("-p", "--port", type=int, default=8001, help="port of the config server")
+    parser.add_argument("--host", type=str, default="0.0.0.0", help="host of the config server")
     args = vars(parser.parse_args())
-    main(args["config"])
+    main(args["config"], args["host"], args["port"])
--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -3,28 +3,23 @@ import threading
 import time
 import logging
 
-from utilities.classLoader import ClassLoader
 from sink.interface.sink import Sink
 from source.interface.source import Source
 from step.interface.operation import Operation
 from model.model import Frame, Result
-from utilities.classLoader import ClassLoader
-from model.observer.observer import Observer
-from model.observer.subject import Subject
-from config.config import ConfigManager
 from model.detection import Detection
 
 logger = logging.getLogger(__name__)
 
 
-
-class Pipeline(Observer):
-    def __init__(self, queue: Queue):
+class Pipeline:
+    def __init__(self, queue: Queue, instances):
 
         self._queue = queue
         self._source: Source|None = None 
-        self._step: Operation|None = None 
+        self._pipe: Operation|None = None 
         self._sinks: list[Sink] = []
+        self._instances = instances
 
         self._stop_event = threading.Event()
         self._source_thread = None
@@ -47,8 +42,8 @@ class Pipeline(Observer):
             frame: Frame = self._queue.get()
             det: list[Detection] = []
 
-            if self._step:
-                det = self._step.process(frame.frame)
+            if self._pipe:
+                det = self._pipe.process(frame.frame)
             result = Result(frame)
             result.add_detection(det)
 
@@ -64,7 +59,6 @@ class Pipeline(Observer):
 
         logger.info("Starting Pipeline thread")
         self._stop_event.clear()
-
         self._running = True
         self._source_thread = threading.Thread(target=self._run_source)
         self._source_thread.start()
@@ -72,69 +66,137 @@ class Pipeline(Observer):
 
 
     def stop(self):
-        logger.info("Stopping Pipeline thread")
+        logger.info("Stopping Pipeline threads")
         self._stop_event.set()
         if self._source_thread:
             self._source_thread.join()
 
 
+    def _restart_source_thread(self):
+            self._stop_event.clear()
+            self._source_thread = threading.Thread(target=self._run_source)
+            self._source_thread.start()
 
 
-    def update(self, subject: Subject) -> None:
-        logger.info("Pipeline received update")
-        assert isinstance(subject, ConfigManager)
+    def set_source(self, source_name: str) -> bool:
 
-        new_config = subject.get_config()
+        if self._source is not None and self._source.get_name() == source_name:
+            logger.info(f"Update source for {source_name}: nothing to change")
+            return True
 
-        # Determine current names
-        current_source_name = self._source.get_name() if self._source else None
-        current_step_name = self._step.get_name() if self._step else None
+        self._stop_event.set()
+
+        if self._source:
+            self._source.release()
+        
+        sources = self._instances.get("sources")
+        instance = sources.get(source_name)
+
+        if instance is not None:
+            self._source = instance
+            instance.init()
+            self._restart_source_thread()
+            logger.info(f"Update source to {source_name}")
+            return True
+
+        logger.warning(f"Could not update source to {source_name}")
+        return False
+
+
+    def set_pipe(self, pipe_name: str) -> bool:
+        if self._pipe is not None and self._pipe.get_name() == pipe_name:
+            logger.info(f"Update pipe to {pipe_name}: nothing to change")
+            return True
+
+        pipes = self._instances.get("pipes")
+        new_pipe = pipes.get(pipe_name)
+        if new_pipe is not None:
+            self._pipe = new_pipe
+            logger.info(f"Update pipe to {pipe_name}")
+            return True
+
+        logger.warning(f"Could not update pipe to {pipe_name}")
+        return False
+
+
+    def set_sinks(self, sink_names: list[str]) -> bool:
         current_sink_names = [sink.get_name() for sink in self._sinks] if self._sinks else []
 
-        # Get new names from config
-        new_source_name = new_config['sources'][0]['name'] if new_config['sources'] else None
-        new_step_name = new_config['steps'][0]['name'] if new_config['steps'] else None
-        new_sink_names = [sink['name'] for sink in new_config['sinks']]
-
-        print("Pipeline update:")
-        print("old: ", current_source_name, current_step_name, current_sink_names)
-        print("new: ", new_source_name, new_step_name, new_sink_names)
-
-        # Identify changes
-        if current_source_name != new_source_name:
-            self._stop_event.set()
-            if self._source:
-                logger.info(f"Removing source: {current_source_name}")
-                self._source.release()
-                self._source = None
-            if new_source_name:
-                logger.info(f"Loading source: {new_source_name}")
-                self._source = ClassLoader.instantiate_class(new_config['sources'][0])
-
-        if current_step_name != new_step_name:
-            if self._step:
-                logger.info(f"Removing step: {current_step_name}")
-                self._step = None
-            if new_step_name:
-                logger.info(f"Loading step: {new_step_name}")
-                step_config = new_config['steps'][0]
-                self._step = ClassLoader.instantiate_class(step_config)
-
+        # remove sinks
         for sink in self._sinks:
-            if sink.get_name() not in new_sink_names:
-                logger.info(f"Removing sink: {sink.get_name()}")
+            if sink.get_name() not in sink_names:
                 sink.release()
-        self._sinks = [
-            sink for sink in self._sinks if sink.get_name() in new_sink_names
-        ]
+                logger.info(f"remove sink {sink.get_name()}")
+                sink.release()
+                self._sinks.remove(sink)
 
-        for sink_config in new_config['sinks']:
-            if sink_config['name'] not in current_sink_names:
-                logger.info(f"Loading sink: {sink_config['name']}")
-                self._sinks.append(ClassLoader.instantiate_class(sink_config))
+        # add new sinks
+        for sink_name in sink_names:
+            if sink_name not in current_sink_names:
+                sinks = self._instances.get("sinks")
+                instance = sinks.get(sink_name)
+                if instance is not None:
+                    logger.info(f"add sink: {sink_name}")
+                    instance.init()
+                    self._sinks.append(instance)
+        return True
+
+
+    # def update(self, subject: Subject) -> None:
+    #     logger.info("Pipeline received update")
+    #     assert isinstance(subject, ConfigManager)
+
+    #     new_config = subject.get_config()
+
+    #     # Determine current names
+    #     current_source_name = self._source.get_name() if self._source else None
+    #     current_step_name = self._step.get_name() if self._step else None
+    #     current_sink_names = [sink.get_name() for sink in self._sinks] if self._sinks else []
+
+    #     # Get new names from config
+    #     new_source_name = new_config['sources'][0]['name'] if new_config['sources'] else None
+    #     new_step_name = new_config['steps'][0]['name'] if new_config['steps'] else None
+    #     new_sink_names = [sink['name'] for sink in new_config['sinks']]
+
+    #     print("Pipeline update:")
+    #     print("old: ", current_source_name, current_step_name, current_sink_names)
+    #     print("new: ", new_source_name, new_step_name, new_sink_names)
+
+    #     # Identify changes
+    #     if current_source_name != new_source_name:
+    #         self._stop_event.set()
+    #         if self._source:
+    #             logger.info(f"Removing source: {current_source_name}")
+    #             self._source.release()
+    #             self._source = None
+    #         if new_source_name:
+    #             logger.info(f"Loading source: {new_source_name}")
+    #             self._source = ClassLoader.instantiate_class(new_config['sources'][0])
+
+    #     if current_step_name != new_step_name:
+    #         if self._step:
+    #             logger.info(f"Removing step: {current_step_name}")
+    #             self._step = None
+    #         if new_step_name:
+    #             logger.info(f"Loading step: {new_step_name}")
+    #             step_config = new_config['steps'][0]
+    #             self._step = ClassLoader.instantiate_class(step_config)
+
+    #     for sink in self._sinks:
+    #         if sink.get_name() not in new_sink_names:
+    #             logger.info(f"Removing sink: {sink.get_name()}")
+    #             sink.release()
+    #     self._sinks = [
+    #         sink for sink in self._sinks if sink.get_name() in new_sink_names
+    #     ]
+
+    #     for sink_config in new_config['sinks']:
+    #         if sink_config['name'] not in current_sink_names:
+    #             logger.info(f"Loading sink: {sink_config['name']}")
+    #             self._sinks.append(ClassLoader.instantiate_class(sink_config))
 
 
 
-        self._stop_event = threading.Event()
-        self._source_thread = threading.Thread(target=self._run_source)
-        self._source_thread.start()
+    #     self._stop_event = threading.Event()
+    #     self._source_thread = threading.Thread(target=self._run_source)
+    #     self._source_thread.start()
